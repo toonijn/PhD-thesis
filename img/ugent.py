c8ccc7bb18ed9bc15c4a8bd88c3d5a6d4f6687e5
Toon Baeyens <toon.baeyens@gmail.com>
2022-06-10 18:28:26 +0200

    flow of eigenvalues on disc and moons

---
+++ img/ugent.py
@@ -0,0 +1,66 @@
+from matplotlib import cm
+import matplotlib.colors as mc
+import itertools
+from math import pi
+
+kleuren = {
+    'wit': "#FFFFFF",
+    'zwart': "#000000",
+    "blauw": "#1E64C8",
+    'geel': "#FFD200",
+    "oranje": "#F1A42B",
+    "rood": "#DC4E28",
+    "aqua": "#2D8CA8",
+    "roze": "#E85E71",
+    "hemelsblauw": "#8BBEE8",
+    "lichtgroen": "#AEB050",
+    "paars": "#825491",
+    "warmoranje": "#FB7E3A",
+    "turquoise": "#27ABAD",
+    "lichtpaars": "#BE5190",
+    "groen": "#71A860",
+}
+
+for k in kleuren:
+    kleuren[k] = mc.to_rgb(kleuren[k])
+
+globals().update(kleuren)
+
+rainbow_kleuren = [
+    'blauw', 'rood', 'geel', 'paars', 'groen', 'warmoranje', 'aqua', 'lichtpaars', 'hemelsblauw'
+]
+rainbow_kleuren.append(rainbow_kleuren[0])
+
+RdBu = mc.LinearSegmentedColormap.from_list("ugent_RdBu", list(map(kleuren.get, [
+    'rood', 'wit', 'blauw'])))
+Rainbow = mc.LinearSegmentedColormap.from_list(
+    "ugent_Rainbow", list(map(kleuren.get, rainbow_kleuren)))
+
+
+class LazyMap:
+    def __init__(self, f):
+        self.f = f
+
+    def __getitem__(self, i):
+        return self.f(i)
+
+    def __iter__(self):
+        return map(self.f, itertools.count())
+
+
+palet_N = len(rainbow_kleuren)-1
+palet = LazyMap(lambda x: Rainbow(((x / palet_N % 1) + pi**(x//palet_N)) % 1))
+
+cm.register_cmap("ugent_RdBu", RdBu)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    ts = np.linspace(0, 10, 200)
+
+    plt.figure()
+    for kleur, i in zip(palet, range(20)):
+        plt.plot(ts, np.sin(ts + 0.1*i)+i, color=kleur)
+    plt.show()
