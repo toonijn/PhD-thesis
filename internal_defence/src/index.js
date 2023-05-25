c8e816798bb3753b0edbeb7614b596ef3d666bc3
Toon Baeyens <toon.baeyens@gmail.com>
2023-05-25 18:31:35 +0200

    prepare internal defence

---
+++ internal_defence/src/index.js
@@ -0,0 +1,10 @@
+import Reveal from "reveal.js";
+
+let deck = new Reveal({
+  hash: true,
+  slideNumber: true,
+  history: true,
+  progress: false,
+  controlsTutorial: false,
+});
+deck.initialize();
