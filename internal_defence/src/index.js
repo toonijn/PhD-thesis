f1e801bde6af5222b6abbc38598f837c6ee46b02
Toon Baeyens <toon.baeyens@ugent.be>
2023-05-29 11:33:38 +0200

    internal defence: slides about bug

--- internal_defence/src/index.js
+++ internal_defence/src/index.js
@@ -1,4 +1,5 @@
 import Reveal from "reveal.js";
+import RevealMath from "reveal.js/plugin/math/math.js";
 
 let deck = new Reveal({
   hash: true,
@@ -6,5 +7,10 @@ let deck = new Reveal({
   history: true,
   progress: false,
   controlsTutorial: false,
+  plugins: [RevealMath.KaTeX],
+  center: false,
+  width: 1024,
+  height: 576,
+  margin: 0.1,
 });
 deck.initialize();
