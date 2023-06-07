5db94f7210dfe3333e9705f3af267deb3843db55
Toon Baeyens <toon.baeyens@gmail.com>
2023-06-07 16:25:46 +0200

    start public

---
+++ public_defence/src/index.js
@@ -0,0 +1,17 @@
+import Reveal from "reveal.js";
+import RevealMath from "reveal.js/plugin/math/math.js";
+
+let deck = new Reveal({
+  hash: true,
+  slideNumber: true,
+  history: true,
+  progress: false,
+  controlsTutorial: false,
+  plugins: [RevealMath.KaTeX],
+  center: false,
+  width: 1024,
+  height: 576,
+  margin: 0,
+  maxScale: 3,
+});
+deck.initialize();
